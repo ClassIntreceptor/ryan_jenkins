92a222e005e6837ec996ddcff04dde3973f0438c
Ryan <13966736115@163.com>
2022-06-03 00:07:43 +0800

    1.首次提交代码

---
+++ src/main/java/com/ryan/jenkins/RyanJenkinsApplication.java
@@ -0,0 +1,16 @@
+package com.ryan.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: Ryan
+ * @time: 2022/6/2 23:34
+ */
+@SpringBootApplication
+public class RyanJenkinsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RyanJenkinsApplication.class, args);
+    }
+}
